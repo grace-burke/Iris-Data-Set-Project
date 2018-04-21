c98f0bb90ad30c22420bde47b7b760e9c429169b
Grace Burke <g00364750@gmit.it>
2018-04-21 17:33:40 +0100

    Preliminary analysis

--- IrisAnalysis.py
+++ IrisAnalysis.py
@@ -1,17 +1,24 @@
 # Iris Data Set
 # http://archive.ics.uci.edu/ml/datasets/Iris
 
-with open("Data/IrisData.csv") as Iris:
-    # The with keyword allows the data to be called from the address given, but only for the indented code. The data file is closed after this.
+# https://www.kaggle.com/ashokdavas/iris-data-analysis-pandas-numpy
 
-    for line in Iris:
-        col1 = (line.split(',')[0])
-        col2 = (line.split(',')[1])
-        col3 = (line.split(',')[2])
-        col4 = (line.split(',')[3])
-        # For ease of reading, each of the columns is named separately.
-        # For each line, the value up to the first comma is placed in col1, the value between the first and second comma is placed in col2, and so on.
-        
-        print('{:4s} {:4s} {:4s} {:4s}'\
-        .format(col1,col2,col3,col4))
-        # This formats the string values in col1 to col4 to contain a minimum of 4 characters.
+import pandas as pd
+import numpy as np
+
+with open('Data/IrisData.csv') as f:
+    column_names = ['Sepal Length','Sepal Width','Petal Length','Petal Width','Class']
+    Iris = pd.read_csv(f, header=None,names=column_names)
+    # https://pandas.pydata.org/pandas-docs/stable/generated/pandas.read_csv.html
+    Iris.index = np.arange(1,len(Iris)+1)
+    # https://stackoverflow.com/questions/32249960/in-python-pandas-start-row-index-from-1-instead-of-zero-without-creating-additi
+
+    for column in Iris:
+                print(Iris[column].max())
+                print(Iris[column].min())
+                print(Iris[column].mean())
+    
+    # print(Iris)
+
+    # print(Iris.at[3,'Sepal Width'])
+    # https://www.datacamp.com/community/tutorials/pandas-tutorial-dataframe-python
